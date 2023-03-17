665304baacf35c980a7a9f5603ed15fba61159c4
Gustavo  Alves da Silva <gustavoalves.silva1994@gmail.com>
2023-03-17 16:35:47 +0000

    Primeiro exercício sobre estruturas de repetição

--- src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
+++ src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
@@ -2,29 +2,36 @@ package br.com.dio.exercicios.loops;
 
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 /*
 Faça um programa que leia conjuntos de dois valores,
 o primeiro representando o nome do aluno e o segundo representando a sua idade.
 (Pare o programa inserindo o valor 0 no campo nome)
 */
 public class Ex1_NomeEIdade {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-        String nome;
-        int idade;
+    String nome;
+    int idade;
 
-        while(true) {
-            System.out.println("Nome: ");
-            nome = scan.next();
-            if (nome.equals("0")) break;
+    while(true) {
+    System.out.println("Nome: ");
+    nome = scan.next();
+    if (nome.equals("0")) break;
 
-            System.out.println("Idade: ");
-            idade = scan.nextInt();
-        }
-
-        System.out.println("Continua aqui...");
-
+    System.out.println("Idade: ");
+    idade = scan.nextInt();
     }
+
+    System.out.println("Continua aqui...");
+    
+
+
+
+
+
+   }
 
 }
