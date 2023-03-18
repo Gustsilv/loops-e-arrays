df5883bcc28cea74467153206757708a36cba529
Gustavo  Alves da Silva <gustavoalves.silva1994@gmail.com>
2023-03-18 16:42:05 +0000

    Pratica do 3° Exercício de Arrays

--- src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
+++ src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
@@ -7,34 +7,37 @@ Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) arma
 Ao final mostre os números e seus sucessores.
 */
 public class Ex3_NumerosAleatorios {
-    public static void main(String[] args) {
-        Random random = new Random();
+   public static void main(String[] args) {
+        Random ramdom = new Random();
+
 
         int[] numerosAleatorios = new int[20];
 
+
         for(int i = 0; i < numerosAleatorios.length; i++) {
-            int numero = random.nextInt(100);
+            int numero = ramdom.nextInt(100);
             numerosAleatorios[i] = numero;
         }
 
-        System.out.print("Numeros Aleatorios: ");
+        System.out.println("Números Aleatórios: ");
         for (int numero : numerosAleatorios) {
-            System.out.print(numero + " ");
+            System.out.println(numero + " ");
+            
         }
 
-        System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+        System.out.print("\nAntecessor dos Números Aleatórios: ");
         for (int numero : numerosAleatorios) {
-            System.out.print((numero-1) + " ");
+            System.out.println(numero-1 + " ");
+            
         }
 
-        System.out.print("\nSucessores dos Numeros Aleatorios: ");
+        System.out.print("\nSucessores dos Números Aleatórios: ");
         for (int numero : numerosAleatorios) {
-            System.out.print((numero+1) + " ");
+            System.out.println(numero+1 + " ");
+            
         }
 
-
-
-
-    }
+        
+   }
 
 }
